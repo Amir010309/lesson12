520d65ca4dd61752db3c1a51455a93a9226aeb55
Amir010309 <164783967+Amir010309@users.noreply.github.com>
2024-06-07 18:19:09 +0300

    Add files via upload

---
+++ lesson12.py
@@ -0,0 +1,25 @@
+import random
+
+def matrix1(x):
+    for i in x:
+        print(i)
+
+
+matrix1 = [[random for _ in range(4)] for i in range(4)]
+print(matrix1)
+
+def matrix2(y):
+    for i in y:
+        print(i)
+
+
+matrix2 = [[random for _ in range(4)] for i in range(4)]
+print(matrix2)
+
+matrix3 = [[0 for j in range (4)] for i in range(4)]
+
+for i in range(len(matrix1)):
+    for j in range (len(matrix1[0])):
+        matrix3[i][j] = matrix1[i][j] + matrix2[i][j]
+        
+        print(matrix3)
